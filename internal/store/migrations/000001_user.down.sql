19509cb2ee2e89b3b814c8d5135a118af77fdd06
oseayemenre <oseayemenre04@gmail.com>
2025-05-29 20:54:19 +0100

    user schema

---
+++ internal/store/migrations/000001_user.down.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS user; 
+DROP FUNCTION IF EXISTS update_user_table_updated_at; 
+DROP TRIGGER update_user_table_updated_at_trigger ON user;
---
+++ internal/store/migrations/000001_user.up.sql
@@ -0,0 +1,21 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users(
+  id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+  name TEXT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE OR REPLACE FUNCTION update_user_table_updated_at()
+RETURNS TRIGGER
+AS $$
+BEGIN
+  NEW.updated_at := NOW();
+  RETURN NEW;
+END $$ language plpgsql;
+
+CREATE TRIGGER update_user_table_updated_at_trigger
+BEFORE UPDATE on users
+FOR EACH ROW
+  EXECUTE FUNCTION update_user_table_updated_at();
