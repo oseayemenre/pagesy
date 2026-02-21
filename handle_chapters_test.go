bb928d6a2d24f79e1e5e2a442881e834b648f138
oseayemenre <oseayemenre04@gmail.com>
2026-02-20 19:13:52 -0800

    .

--- handle_chapters_test.go
+++ handle_chapters_test.go
@@ -171,5 +171,83 @@ func TestHandleGetChapter(t *testing.T) {
 			}
 		})
 	}
+}
 
+func TestHandleDeleteChapter(t *testing.T) {
+	db := connectTestDb(t)
+	userID := createAndCleanUpUser(t, db)
+	token, err := createJWTToken(userID)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	svr := newServer(nil, db, nil, nil)
+	bookID := createBook(t, userID, db)
+	chapterID, err := svr.uploadChapter(context.Background(), userID, &chapter{title: "test chapter", chapterNo: 1, content: "test chapter content", bookID: bookID})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	tests := []struct {
+		name         string
+		cookieName   string
+		cookieValue  string
+		bookID       string
+		chapterID    string
+		mockChannel  channel
+		expectedCode int
+	}{
+		{
+			name:         "no access token cookie",
+			bookID:       uuid.NewString(),
+			chapterID:    uuid.NewString(),
+			expectedCode: http.StatusNotFound,
+		},
+		{
+			name:         "invalid/malformed token",
+			cookieName:   "access_token",
+			cookieValue:  "invalid token",
+			bookID:       uuid.NewString(),
+			chapterID:    uuid.NewString(),
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:         "book not found",
+			cookieName:   "access_token",
+			cookieValue:  token,
+			bookID:       uuid.NewString(),
+			chapterID:    uuid.NewString(),
+			expectedCode: http.StatusNotFound,
+		},
+		{
+			name:         "chapter not found",
+			cookieName:   "access_token",
+			cookieValue:  token,
+			bookID:       bookID,
+			chapterID:    uuid.NewString(),
+			expectedCode: http.StatusNotFound,
+		},
+		{
+			name:         "delete book",
+			cookieName:   "access_token",
+			cookieValue:  token,
+			bookID:       bookID,
+			chapterID:    chapterID,
+			expectedCode: http.StatusNoContent,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			r := httptest.NewRequest(http.MethodDelete, fmt.Sprintf("/api/v1/books/%v/chapters/%v", tc.bookID, tc.chapterID), nil)
+			r.AddCookie(&http.Cookie{Name: tc.cookieName, Value: tc.cookieValue})
+			rr := httptest.NewRecorder()
+
+			svr.router.ServeHTTP(rr, r)
+
+			if rr.Code != tc.expectedCode {
+				t.Fatalf("expected %d, got %d", tc.expectedCode, rr.Code)
+			}
+		})
+	}
 }
